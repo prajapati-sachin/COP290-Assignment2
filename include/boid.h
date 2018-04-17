cb73c4e1e37c11bd784a6c515061985fcc8cf307
pranavbhagat5 <pranavbhagat5@outlook.com>
2018-04-18 01:01:21 +0530

    created boid.h

---
+++ include/boid.h
@@ -0,0 +1,20 @@
+#ifndef BOID
+#define BOID
+
+#include <vector>
+
+struct position{
+	float x; //x-coordinate
+	float y; //y-coordinate
+	float z; //z-coordinate
+}
+
+struct direction{
+	float i; //i-direction
+	float j; //j-direction
+	float k; //k-direction
+}
+
+class boid{
+
+}
