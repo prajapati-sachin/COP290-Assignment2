1211356a375e456ac5c4795aa40d3f1c3b0b6e5d
pranavbhagat5 <pranavbhagat5@outlook.com>
2018-04-18 11:25:42 +0530

    modified boid.h

--- include/boid.h
+++ include/boid.h
@@ -2,19 +2,44 @@
 #define BOID
 
 #include <vector>
+#include "math."
 
-struct position{
+extern float RADIUS;
+extern float Align_Radius;
+extern float Cohese_Radius;
+extern float Seperation_Radius;
+
+struct Position{
 	float x; //x-coordinate
 	float y; //y-coordinate
 	float z; //z-coordinate
 }
 
-struct direction{
+struct Direction{
 	float i; //i-direction
 	float j; //j-direction
 	float k; //k-direction
 }
 
-class boid{
+class Boid{
+	public:
 
+	Position location;
+
+	Direction direction;
+
+	float speed;
+
+	vector<Boid> neighbours;
+
+	Boid():
+
+	void findneighbours(vector<Boid> all);
+
+	Direction get_average_direction();
+
+	Position get_average_location();
 }
+
+
+float distance_boids(Boid boid1,Boid boid2);
